106aeca71d839560ce18d73e0e6b122e8e9b46b3
Brendan Sudol <brendan.sudol@gsa.gov>
2017-03-24 10:59:48 -0400

    add table, better search

--- demos/ori/components/App.js
+++ demos/ori/components/App.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 import React from 'react'
 
+import Table from './Table'
+
+
 class App extends React.Component {
-  state = { data: [], search: '' }
+  state = { data: [], search: '', isFetching: true }
 
   componentDidMount() {
-    axios.get('/data/ori_ny.json')
+    axios.get('/data/ori.json')
       .then(response => response.data)
-      .then(data => this.setState({ data }))
+      .then(data => this.setState({ data, isFetching: false }))
       .catch(error => console.log(error))
   }
 
@@ -16,10 +19,17 @@ class App extends React.Component {
   }
 
   render() {
-    const { data, search } = this.state
-    const dataFiltered = data.filter(d => (
-      d['NAME'].includes(search.toUpperCase())
-    ))
+    const { data, search, isFetching } = this.state
+
+    const searchUpper = search.toUpperCase()
+    const dataFiltered = data.filter(d => {
+      const words = `
+        ${d['NAME']} ${d['COUNTYNAME']} ${d['STATENAME']}
+        ${d['ADDRESS_ZIP']} ${d['ADDRESS_CITY']}
+      `.toUpperCase()
+
+      return words.includes(searchUpper)
+    })
 
     return (
       <div className='px2 py3 container'>
@@ -30,10 +40,18 @@ class App extends React.Component {
           value={search}
           onChange={this.handleChange}
         />
-        <h2 className='mt0'>{dataFiltered.length} results</h2>
-        {dataFiltered.slice(0, 100).map((d, i) => (
-          <p key={i}>{JSON.stringify(d)}</p>
-        ))}
+        {isFetching ? <div>Loading...</div> : (
+          <div>
+            <h3 className='mt0'>{dataFiltered.length} results</h3>
+            <Table
+              data={dataFiltered.slice(0, 100)}
+              keys={[
+                'ORI7', 'ORI9', 'NAME', 'ADDRESS_CITY',
+                'COUNTYNAME', 'ADDRESS_ZIP', 'STATENAME'
+              ]}
+            />
+          </div>
+        )}
       </div>
     )
   }
