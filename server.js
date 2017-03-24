ffd33354aca6e818d63f70a8ed1160a6911dc257
Brendan Sudol <brendan.sudol@gsa.gov>
2017-03-24 01:26:34 -0400

    add ori data, tweak index

--- server.js
+++ server.js
@@ -12,26 +12,30 @@ const credService = env.getService('crime-data-api-creds') || { credentials: {} 
 const username = credService.credentials["HTTP_BASIC_USERNAME"]
 const password = credService.credentials["HTTP_BASIC_PASSWORD"]
 
+app.use(express.static(__dirname))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(basicAuth(username, password))
+}
+
 const getDemos = () => {
   const where = path.join(__dirname, 'demos')
   return fs.readdirSync(where)
     .filter(f => fs.statSync(path.join(where, f)).isDirectory())
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(basicAuth(username, password))
-}
-
-app.use(express.static(__dirname))
-
 app.get('/', (req, res) => {
-  const demos = getDemos()
-  const html = `<h1>FBI Crime Data Explorer prototypes</h1>
-  <ul>
-    ${demos.map(d => (
-      `<li><a href='/demos/${d}/'>${d}</a></li>`
+  const html = `
+    <!DOCTYPE html>
+    <style>
+    body{margin:0;padding:32px;font-family:courier,monospace;line-height:1.5;}
+    h1{font-size:20px;}
+    </style>
+    <h1>FBI Crime Data Explorer prototypes</h1>
+    ${getDemos().map(d => (
+      `<div><a href='/demos/${d}/'>${d}</a></div>`
     )).join('')}
-  </ul>`
+  `
   res.send(html)
 })
 
