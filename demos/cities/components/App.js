fb8ce16177862c135d13bf604df72042dcc1aa81
Brendan Sudol <brendan.sudol@gsa.gov>
2017-04-03 10:40:35 -0400

    map w/ cities iterate

--- demos/cities/components/App.js
+++ demos/cities/components/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 
 import Map from './Map'
-import cities from '../data/cities.json'
-import states from '../data/states.json'
 
 class App extends React.Component {
   render() {
     return (
-      <div className='p3'>
+      <div className='container p2' style={{ maxWidth: 900 }}>
         <Map />
-        <h2>states:</h2>
-        <pre>{JSON.stringify(states, null, 2)}</pre>
-        <h2>cities:</h2>
-        <pre>{JSON.stringify(cities, null, 2)}</pre>
       </div>
     )
   }
--- demos/cities/components/Map.js
+++ demos/cities/components/Map.js
@@ -1,12 +1,19 @@
+import axios from 'axios'
+import { geoAlbersUsa, geoPath } from 'd3-geo'
 import React from 'react'
+import { feature, mesh } from 'topojson'
 
-import cities from '../data/cities.json'
-import states from '../data/states.json'
+import Hint from './Hint'
 
 class Map extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { hover: null }
+    this.state = { hover: null, usa: null }
+  }
+
+  componentDidMount() {
+    axios.get('data/usa.json')
+      .then(response => { this.setState({ usa: response.data }) })
   }
 
   rememberValue = id => e => {
@@ -20,27 +27,74 @@ class Map extends React.Component {
   }
 
   render() {
+    const { selected, usCounty, usCountyUpdate } = this.props
+    const { hover, usa } = this.state
+
+    if (!usa) return null
+
+    const [w, h] = [400, 300]
+    const projection = geoAlbersUsa().scale(500).translate([w / 2, h / 2])
+    const path = geoPath().projection(projection)
+    const meshed = mesh(usa, usa.objects.states, (a, b) => (a !== b))
+
+    const geoStates = feature(usa, usa.objects.states).features
+    const active = geoStates.find(s => (s.properties.name === selected))
+
+    const geoCities = feature(usa, usa.objects.places).features
+    const citiesFiltered = geoCities.filter(c => c.properties.pop > 1000000)
+
+    window.cities = citiesFiltered
+
     return (
       <div>
         <svg
-          viewBox='0 0 959 593'
+          className='pointer'
           preserveAspectRatio='xMidYMid'
+          viewBox={`0 0 ${w} ${h}`}
         >
-          <title>USA</title>
           <g>
-            {states.map(s => (
+            <g className='states'>
+              {geoStates.map((d, i) => (
+                <path
+                  key={i}
+                  d={path(d)}
+                  fill={d.properties.name === selected || !active ? '#95aabc' : '#eff4f9'}
+                  pointerEvents='all'
+                  onMouseOver={this.rememberValue(d.properties.name)}
+                  onMouseMove={this.rememberValue(d.properties.name)}
+                  onMouseOut={this.forgetValue}
+                />
+              ))}
               <path
-                key={s.id}
-                id={s.id}
-                d={s.d}
-                pointerEvents='all'
-                onMouseOver={this.rememberValue(s.name)}
-                onMouseMove={this.rememberValue(s.name)}
-                onMouseOut={this.forgetValue}
+                d={path(meshed)}
+                fill='none'
+                stroke='#fff'
+                strokeWidth='.5'
+                strokeLinecap='round'
+                strokeLinejoin='round'
               />
-            ))}
+            </g>
+            <g className='cities'>
+              {citiesFiltered.map((d, i) => {
+                const info = d.properties
+                return (
+                  <circle
+                    key={i}
+                    transform={`translate(${projection(d.geometry.coordinates)})`}
+                    r='2'
+                    fill='#284152'
+                    stroke='#fff'
+                    strokeWidth='.5'
+                    pointerEvents='all'
+                    onMouseOver={this.rememberValue(`${info.name}, ${info.state}`)}
+                    onMouseOut={this.forgetValue}
+                  />
+                )
+              })}
+            </g>
           </g>
         </svg>
+        {hover ? <Hint {...hover} /> : null}
       </div>
     )
   }
